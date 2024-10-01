00c621150a61e1199258820f8d149ecba28b39de
kuk1157 <kuk1938@naver.com>
2024-10-01 19:37:20 +0900

    feat: 일정 RequestDto
    일정 전체 기능에 대한 RequestDto

--- src/main/java/com/sparta/schedule/dto/ScheduleRequestDto.java
+++ src/main/java/com/sparta/schedule/dto/ScheduleRequestDto.java
@@ -4,10 +4,14 @@ import lombok.Getter;
 
 @Getter
 public class ScheduleRequestDto { // 데이터 한줄
-    // [ 레벨 4때 사용 ]
-//    private int w_idx; // 작성자 고유번호
-//    private String w_name; // 작성자 이름
-    private String memo; // 내용(컬럼)
-    private String pw; // 비밀번호(컬럼)
-//    private String s_pw_check; // 비밀번호확인
+    private int id; // 일정 고유아이디
+    private int w_id; // 작성자 고유아이디
+    private String w_name; // 작성자 이름
+    private String memo; // 내용
+    private String pw; // 비밀번호
+    private String pw_check; // 비밀번호확인
+    private String reg_date; // 작성시간
+    private String edit_date;
+
+    private String name; // 작성자 이름 서브쿼리
 }
