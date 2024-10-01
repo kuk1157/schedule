0125d31ce3626c456c7c50c41f0b56730e75995d
kuk1157 <kuk1938@naver.com>
2024-10-01 19:32:40 +0900

    feat: 일정 entity 파일
    1. 일정관리 전체적인 기능에 대한 entity파일
    ㄴ 일정 메인테이블과 연동 / controller, requestDto, responseDto와 연동

--- src/main/java/com/sparta/schedule/entity/Schedule.java
+++ src/main/java/com/sparta/schedule/entity/Schedule.java
@@ -9,15 +9,28 @@ import lombok.Setter;
 @Setter
 public class Schedule {
 
-    // [ 레벨 4때 사용 ]
-//    private int w_idx; // 작성자 고유번호
-//    private String w_name; // 작성자 이름
+    private int id; // 일정 고유아이디
+    private int w_id; // 작성자 고유아이디
+    private String w_name; // 작성자 이름
     private String memo; // 내용
     private String pw; // 비밀번호
+    private String pw_check; // 비밀번호확인
+    private String reg_date; //
+    private String edit_date;
+
+    private String name; // 작성자 이름 서브쿼리
+
 
     public Schedule(ScheduleRequestDto scheduleRequestDto) {
+        this.id = scheduleRequestDto.getId();
+        this.w_id = scheduleRequestDto.getW_id();
+        this.w_name = scheduleRequestDto.getW_name();
         this.memo = scheduleRequestDto.getMemo();
         this.pw = scheduleRequestDto.getPw();
+        this.pw_check = scheduleRequestDto.getPw_check();
+        this.reg_date = scheduleRequestDto.getReg_date();
+        this.edit_date = scheduleRequestDto.getEdit_date();
+        this.name = scheduleRequestDto.getName();
     }
 //    private String s_pw_check; // 비밀번호확인
 
