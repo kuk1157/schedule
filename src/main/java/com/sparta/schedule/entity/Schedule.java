686f52c74ca1f9ec65eca9c4f99aa767be497775
kuk1157 <kuk1938@naver.com>
2024-10-03 17:37:43 +0900

    style: entity 확인만 하였음
    entity 확인만 하였음

--- src/main/java/com/sparta/schedule/entity/Schedule.java
+++ src/main/java/com/sparta/schedule/entity/Schedule.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+// 일정 테이블 전체
 public class Schedule {
 
     private int id; // 일정 고유아이디
@@ -15,11 +16,12 @@ public class Schedule {
     private String memo; // 내용
     private String pw; // 비밀번호
     private String pw_check; // 비밀번호확인
-    private String reg_date; //
-    private String edit_date;
+    private String reg_date; // 작성일
+    private String edit_date; // 수정일
 
     private String name; // 작성자 이름 서브쿼리
-
+    private int count; // 일정 데이터 개수
+//    private String s_pw_check; // 비밀번호확인
 
     public Schedule(ScheduleRequestDto scheduleRequestDto) {
         this.id = scheduleRequestDto.getId();
@@ -31,7 +33,8 @@ public class Schedule {
         this.reg_date = scheduleRequestDto.getReg_date();
         this.edit_date = scheduleRequestDto.getEdit_date();
         this.name = scheduleRequestDto.getName();
+        this.count = scheduleRequestDto.getCount();
     }
-//    private String s_pw_check; // 비밀번호확인
+
 
 }
