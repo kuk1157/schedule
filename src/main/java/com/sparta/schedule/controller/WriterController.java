d8f2239162e82f09dac8863d2303acc45df24f3b
kuk1157 <kuk1938@naver.com>
2024-10-03 17:42:23 +0900

    perf: 3 layer Architecture 구성
    3 layer Architecture 구성

--- src/main/java/com/sparta/schedule/controller/WriterController.java
+++ src/main/java/com/sparta/schedule/controller/WriterController.java
@@ -1,6 +1,8 @@
 package com.sparta.schedule.controller;
 
 import com.sparta.schedule.dto.WriterResponseDto;
+import com.sparta.schedule.service.ScheduleService;
+import com.sparta.schedule.service.WriterService;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,24 +16,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/")
 public class WriterController {
-
     private final JdbcTemplate jdbcTemplate;
     public WriterController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    // 작성자 명단 조회 - 일정등록 SELECT, 일정 수정 SELECT (레벨 2,3,4 연동)
     @GetMapping("/writer")
     public List<WriterResponseDto> getwriters() {
-        String sql = "SELECT id,name FROM writer";
+        WriterService writerService = new WriterService(jdbcTemplate);
+        return writerService.getwriters();
 
-        return jdbcTemplate.query(sql, new RowMapper<WriterResponseDto>() {
-            @Override
-            public WriterResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                return new WriterResponseDto(id, name);
-            }
-        });
     }
-
 }
